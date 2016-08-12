48750c6148805262ace15f5e617d7ffabd18a0a1
sheyman <sam.heyman@gmail.com>
2016-08-12 17:09:30 +0000

    adding spice from and extra code in handle method to set origin and destination

--- lib/DDG/Spice/SkyscannerFlightSearch.pm
+++ lib/DDG/Spice/SkyscannerFlightSearch.pm
@@ -12,7 +12,8 @@ spice is_cached => 1;
 spice proxy_cache_valid => '200 1d'; # defaults to this automatically
 
 spice wrap_jsonp_callback => 1; # only enable for non-JSONP APIs (i.e. no &callback= parameter)
-spice to => 'http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/GB/GBP/en-GB/$1/anywhere/anytime/anytime?apikey={{ENV{DDG_SPICE_SKYSCANNER_APIKEY}}}';
+spice from => '([^/]*)/([^/]*)';
+spice to => 'http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/GB/GBP/en-GB/$1/$2/anytime/anytime?apikey={{ENV{DDG_SPICE_SKYSCANNER_APIKEY}}}';
 spice alt_to => {
     skyscanner_images => {
         to => 'https://gateway.skyscanner.net/travel-api/v1/entities?external_ids=$1&enhancers=images&apikey={{ENV{DDG_SPICE_SKYSCANNER_IMAGES_APIKEY}}}'
@@ -27,9 +28,19 @@ triggers any => 'skyscanner', 'skycanner flights', 'skyscanner inspire me', 'sky
 # Handle statement
 handle remainder => sub {
     # get user's location for default origin
-    my $location = $loc->country_code;
-    return $location if $location;
-    return;
+    my $origin = "";
+    my $destination = "";
+    
+    # if no origin is specified then use the user's location
+    if ($origin eq "") {
+        $origin = $loc->country_code;
+    }
+    # if no destination is specified then use 'anywhere'
+    if ($destination eq "") {
+        $destination = "anywhere";
+    } 
+    return $origin, $destination;
+    
     # return $_ if $_;
     # Query is in $_ or @_, depending on the handle you chose...if you
     # need to do something with it before returning
